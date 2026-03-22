4ddf954e4a1d3a5c912b1dafb764f213ff049131
berkholz <code@berkholz.org>
2026-03-22 23:57:07 +0100

    add bash script for building binary on *nix

---
+++ build_executable.macosx.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+source .venv/bin/activate
+
+CMD_ARGS="$CMD_ARGS --noconsole"
+CMD_ARGS="$CMD_ARGS --windowed"
+CMD_ARGS="$CMD_ARGS --add-data locale/de/LC_MESSAGES/:locale/de/LC_MESSAGES"
+CMD_ARGS="$CMD_ARGS --add-data locale/en/LC_MESSAGES/:locale/en/LC_MESSAGES"
+CMD_ARGS="$CMD_ARGS --add-data locale/*.pot:locale/"
+CMD_ARGS="$CMD_ARGS EPC-QR-Generator.py"
+
+pyinstaller $CMD_ARGS
+
