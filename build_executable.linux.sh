66a0563b92023b5751cc742a19b2f078e3049b03
berkholz <code@berkholz.org>
2026-04-08 01:52:16 +0200

    add two options for pyinstaller cause of errors

--- build_executable.linux.sh
+++ build_executable.linux.sh
@@ -5,6 +5,8 @@ CMD_ARGS="$CMD_ARGS --onefile"
 CMD_ARGS="$CMD_ARGS --add-data locale/de/LC_MESSAGES/:locale/de/LC_MESSAGES"
 CMD_ARGS="$CMD_ARGS --add-data locale/en/LC_MESSAGES/:locale/en/LC_MESSAGES"
 CMD_ARGS="$CMD_ARGS --add-data locale/*.pot:locale/"
+CMD_ARGS="$CMD_ARGS --hidden-import=tkinter"
+CMD_ARGS="$CMD_ARGS --hidden-import=PIL._tkinter_finder"
 CMD_ARGS="$CMD_ARGS EPC-QR-Generator.py"
 
 pyinstaller $CMD_ARGS
